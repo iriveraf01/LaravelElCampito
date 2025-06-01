d105eaba332c8eab0da4f773efc73b17241cf996
israelrfp <iriveraf01@suarezdefigueroa.es>
2025-06-01 17:29:47 +0200

    Ultimando v4

--- resources/views/apartamentos/show.blade.php
+++ resources/views/apartamentos/show.blade.php
@@ -278,6 +278,23 @@
                                 Reservar ahora
                             </button>
                         </form>
+                        <!-- Información adicional -->
+                        <div class="px-6 pb-6">
+                            <div class="border-t border-gray-200 pt-4">
+                                <p class="text-sm text-gray-500 flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    Cancelación gratuita hasta 48h antes
+                                </p>
+                                <p class="text-sm text-gray-500 flex items-center mt-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    Pago en el establecimiento
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
